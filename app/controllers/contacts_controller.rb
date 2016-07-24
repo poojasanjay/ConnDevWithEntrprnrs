85ae4473418dfc43463f07a771e65a56ce672347
poojasanjay <r.pooja111@gmail.com>
2016-07-24 07:19:56 +0000

    Added validation,create action,mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,24 @@ class ContactsController <ApplicationController
         @contact = Contact.new
     end
     def create
+        @contact = Contact.new(contact_params)
+       
+        if @contact.save
+            
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name,email,body).deliver
+            flash[:success] = 'Message sent.'
+            redirect_to new_contact_path
+        else
+            flash[:danger] = 'Error occured,Message has been not sent.'
+            redirect_to new_contact_path
+        end
+        private
+        def contact_params
+            params.require(:contact).permit(:name,:email,:comments)
+        end    
     end
 end
